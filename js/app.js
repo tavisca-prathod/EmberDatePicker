0823632441a3292152dac99e454f8996e9523e96
sandeep <sandyr39@gmail.com>
2014-09-24 23:20:02 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+App = Ember.Application.create();
+
+App.LOG_TRANSITIONS_INTERNAL = true;
+
+App.Router.map(function(){
+    this.resource('datePicker');
+});
+
+App.DatePickerRoute = Em.Route.extend({
+    model : function(){
+        return {"inputDate" : "01-01-2000"};
+    }
+});
+
+var container = Ember.ContainerView.create();
+container.append();
+
+
+
+App.DatePickerView = Ember.View.extend({
+    inputDate: null,
+    tagName : 'input',
+    maxDate : "",
+    classNames: ['ui-datepicker'],
+    didInsertElement: function() {
+        var self = this;
+        this.$().datepicker({dateFormat:"mm-dd-yy",maxDate: this.get('maxDate')});
+        this.$().datepicker("setDate",self.get('inputDate'));
+        this.$().datepicker("show");
+    }.observes('inputDate'),
+
+    change : function() {
+        this.set('inputDate',this.$().datepicker("getDate"));
+    }
+});
